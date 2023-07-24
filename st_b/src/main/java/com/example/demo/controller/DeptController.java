05494126b1d10be4c9ed73a10f4414890ba1875e
ahyeonyy <qlqlql8448@naver.com>
2023-07-24 16:47:15 +0900

    부서수정 진행중 [이아현]

--- st_b/src/main/java/com/example/demo/controller/DeptController.java
+++ st_b/src/main/java/com/example/demo/controller/DeptController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.DeptDAO_JPA;
@@ -23,6 +24,12 @@ public class DeptController {
 
 	@Autowired
 	private DeptDAO_mb dao_mb;
+	
+	@RequestMapping("updateDept")
+	public ModelAndView update(Dept d) {
+		ModelAndView mav = new ModelAndView("/dept/update");
+		return mav;
+		}
 	
 	@GetMapping("/dept/list")
 	public void list(Model model) {
